885fd0d5a82f2fc51dc0b92184a611284ddc0a18
JSP_MacBookPro <jsp_serenity@icloud.com>
2020-11-01 04:22:20 +0900

    [test] 김서방 테스트 샘플

--- SP/SPTests/SPTests.swift
+++ SP/SPTests/SPTests.swift
@@ -10,35 +10,20 @@ import XCTest
 
 class SPTests: XCTestCase {
     
-    var sp: SP!
+    var p1: 서울에서김서방찾기!
     
     override func setUp() {
         super.setUp()
-        sp = SP()
+        p1 = 서울에서김서방찾기()
     }
-    
+
     override func tearDown() {
-        sp = nil
+        p1 = nil
         super.tearDown()
     }
-    
-    func test_숫자를_잘_더하자() {
-        let r = sp.add(x: 100, y: 100)
-        XCTAssertEqual(r, "200")
-    }
-    
-    func test_최소값도_잘_더합니다() {
-        let r = sp.add(x: 0, y: 0)
-        XCTAssertEqual(r, "0")
-    }
-    
-    func test_음수도_잘_더해야지() {
-        let r = sp.add(x: -10, y: -10)
-        XCTAssertEqual(r, "-20")
-    }
-    
-    func test_최대값을_더하더라도_올바르게_계산되어야_한다() {
-        let r = sp.add(x: Int.max, y: Int.max)
-        XCTAssertEqual(r, "18446744073709552000")
+
+    func test_김서방_찾기() {
+        let r = p1.solution(["Jane", "Kim"])
+        XCTAssertEqual(r, "김서방은 1에 있다")
     }
 }
